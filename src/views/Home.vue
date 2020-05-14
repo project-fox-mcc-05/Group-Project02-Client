e91229da6561c22724adcd74184a6a3148d57689
HafidhFinand <hafidh.finand@gmail.com>
2020-05-14 22:02:53 +0700

    finish home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-<div></div>
+<div>
+    <LoginForm v-show ="!isWaitingRoom"></LoginForm>
+    <WaitingRoom v-show ="isWaitingRoom"></WaitingRoom>
+</div>
 </template>
 
 <script>
+import LoginForm from '@/components/LoginForm.vue'
+import WaitingRoom from '@/components/WaitingRoom.vue'
+
 export default {
+  components: {
+    LoginForm,
+    WaitingRoom
+  },
+  computed: {
+    isWaitingRoom () {
+      return this.$store.state.waitingRoom
+    }
+  }
 }
 </script>
-<style>
 
+<style>
+  * {
+    background-color: #33cc33;
+  }
 </style>
