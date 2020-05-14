7bad746dc651a67ac42f17b236efc2ad8f1fca5c
adityapradipta <adityapradipta94@gmail.com>
2020-05-15 05:08:42 +0700

    added redirect after result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h2>Result</h2>
+    <button class="btn btn-primary btn-lg" @click.prevent="backToHomeTogether">Back Home</button>
   </div>
 </template>
 
 <script>
+import socket from '../config/socket'
+
 export default {
   name: 'Result',
   data () {
@@ -13,6 +16,16 @@ export default {
       highestScore: 0,
       winner: {}
     }
+  },
+  methods: {
+    backToHomeTogether () {
+      socket.emit('backToHomeTogether')
+    }
+  },
+  created () {
+    socket.on('backToHomeTogether', (data) => {
+      this.$router.push('/')
+    })
   }
 }
 </script>
