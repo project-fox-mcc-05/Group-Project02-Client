0894b93e505e981e2b0660d95ab49756bf64c451
HafidhFinand <hafidh.finand@gmail.com>
2020-05-15 07:15:56 +0700

    add audio file

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 export default {
   created () {
     localStorage.clear()
+    const audio = new Audio('https://mp3players.s3.us-east-2.amazonaws.com/lobby-classic-game.mp3')
+    audio.play()
   }
 }
 </script>
